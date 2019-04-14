c0971a23b0ecebeecf9688fb4cfe9e22fbe56261
Mrbysco <Mrbysco@users.noreply.github.com>
2019-04-14 16:43:42 +0200

    Use Teleporter's given WorldServer

--- src/main/java/com/mrbysco/telepastries/util/CakeTeleporter.java
+++ src/main/java/com/mrbysco/telepastries/util/CakeTeleporter.java
@@ -11,7 +11,6 @@ import net.minecraft.init.MobEffects;
 import net.minecraft.init.SoundEvents;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.potion.PotionEffect;
-import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.management.PlayerList;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.BlockPos;
@@ -24,12 +23,10 @@ import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Optional;
 
 public class CakeTeleporter extends Teleporter {
-    private final WorldServer worldServer;
     private BlockPos position;
 
     public CakeTeleporter(WorldServer world, BlockPos position) {
         super(world);
-        this.worldServer = world;
         this.position = position;
     }
 
@@ -44,16 +41,14 @@ public class CakeTeleporter extends Teleporter {
     public void teleportToDimension(EntityPlayer player, int dimension, double x, double y, double z) {
         int oldDimension = player.getEntityWorld().provider.getDimension();
         EntityPlayerMP entityPlayerMP = (EntityPlayerMP) player;
-        MinecraftServer server = entityPlayerMP.getEntityWorld().getMinecraftServer();
-        WorldServer worldServer = server.getWorld(dimension);
         FMLCommonHandler.instance().firePlayerChangedDimensionEvent(player, entityPlayerMP.dimension, dimension);
-        worldServer.playSound(null, x + 0.5D, y + 0.5D, z + 0.5D, SoundEvents.BLOCK_PORTAL_TRAVEL, SoundCategory.MASTER, 0.25F, worldServer.rand.nextFloat() * 0.4F + 0.8F);
+        this.world.playSound(null, x + 0.5D, y + 0.5D, z + 0.5D, SoundEvents.BLOCK_PORTAL_TRAVEL, SoundCategory.MASTER, 0.25F, this.world.rand.nextFloat() * 0.4F + 0.8F);
         if (!player.capabilities.isCreativeMode) {
             player.addPotionEffect(new PotionEffect(MobEffects.RESISTANCE, 200, 200, false, false));
         }
 
-        if (worldServer != null && worldServer.getMinecraftServer() != null) {
-            PlayerList playerList = worldServer.getMinecraftServer().getPlayerList();
+        if (this.world != null && this.world.getMinecraftServer() != null) {
+            PlayerList playerList = this.world.getMinecraftServer().getPlayerList();
             playerList.transferPlayerToDimension(entityPlayerMP, dimension, this);
 
             player.setPositionAndUpdate(x, y, z);
@@ -71,8 +66,8 @@ public class CakeTeleporter extends Teleporter {
 
             if (oldDimension == 1) {
                 player.setPositionAndUpdate(x, y, z);
-                worldServer.spawnEntity(player);
-                worldServer.updateEntityWithOptionalForce(player, false);
+                this.world.spawnEntity(player);
+                this.world.updateEntityWithOptionalForce(player, false);
             }
 
         } else {
@@ -137,7 +132,7 @@ public class CakeTeleporter extends Teleporter {
 
     @Override
     public void placeInPortal(Entity entityIn, float rotationYaw) {
-        this.worldServer.getBlockState(position);
+        this.world.getBlockState(position);
         entityIn.setPosition(position.getX(), position.getY(), position.getZ());
         entityIn.motionX = 0.0D;
         entityIn.motionY = 0.0D;
@@ -173,8 +168,8 @@ public class CakeTeleporter extends Teleporter {
                 protectPlayer(playerMP, new BlockPos(x, y, z));
             } else {
                 double moveFactor = 0.125D;
-                double d0 = MathHelper.clamp(x * moveFactor, worldServer.getWorldBorder().minX() + 16.0D, worldServer.getWorldBorder().maxX() - 16.0D);
-                double d1 = MathHelper.clamp(z * moveFactor, worldServer.getWorldBorder().minZ() + 16.0D, worldServer.getWorldBorder().maxZ() - 16.0D);
+                double d0 = MathHelper.clamp(x * moveFactor, this.world.getWorldBorder().minX() + 16.0D, this.world.getWorldBorder().maxX() - 16.0D);
+                double d1 = MathHelper.clamp(z * moveFactor, this.world.getWorldBorder().minZ() + 16.0D, this.world.getWorldBorder().maxZ() - 16.0D);
                 double d2 = 8.0D;
 
                 d0 = (double)MathHelper.clamp((int)d0, -29999872, 29999872);
@@ -187,21 +182,21 @@ public class CakeTeleporter extends Teleporter {
 
     private void protectPlayer(EntityPlayerMP playerIn, BlockPos position)
     {
-        if (worldServer.provider.getDimension() != 0) {
+        if (this.world.provider.getDimension() != 0) {
             for (int x = -2; x <= 2; x++) {
                 for (int z = -2; z <= 2; z++) {
                     if((x == -2 || x == 2) && (z == -2 || z == 2)) {
                         BlockPos testPos = new BlockPos(position.add(x, 3, z));
-                        if (!worldServer.getBlockState(testPos).isFullBlock() && worldServer.getBlockState(testPos).getMaterial().isLiquid()) {
-                            worldServer.setBlockState(testPos, Blocks.OBSIDIAN.getDefaultState());
+                        if (!this.world.getBlockState(testPos).isFullBlock() && this.world.getBlockState(testPos).getMaterial().isLiquid()) {
+                            this.world.setBlockState(testPos, Blocks.OBSIDIAN.getDefaultState());
                         }
                     } else {
-                        if(worldServer.getBlockState(position.add(x, 3, z)).getMaterial().isLiquid()) {
-                            worldServer.setBlockState(position.add(x, 3, z), Blocks.OBSIDIAN.getDefaultState());
+                        if(this.world.getBlockState(position.add(x, 3, z)).getMaterial().isLiquid()) {
+                            this.world.setBlockState(position.add(x, 3, z), Blocks.OBSIDIAN.getDefaultState());
                         }
                         BlockPos testPos = new BlockPos(position.add(x, -2, z));
-                        if (!worldServer.getBlockState(testPos).isFullBlock() || worldServer.getBlockState(testPos).getMaterial().isLiquid()) {
-                            worldServer.setBlockState(testPos, Blocks.OBSIDIAN.getDefaultState());
+                        if (!this.world.getBlockState(testPos).isFullBlock() || this.world.getBlockState(testPos).getMaterial().isLiquid()) {
+                            this.world.setBlockState(testPos, Blocks.OBSIDIAN.getDefaultState());
                         }
                     }
                 }
@@ -210,12 +205,12 @@ public class CakeTeleporter extends Teleporter {
 
         BlockPos platformPos = new BlockPos(position.add(1, 2, 1));
         for (int y = 1; y <= 3; y++) {
-            if (worldServer.getBlockState(position.add(0, y, 0)).isFullBlock()) {
+            if (this.world.getBlockState(position.add(0, y, 0)).isFullBlock()) {
                 for (int x = -1; x <= 1; x++) {
                     for (int z = -1; z <= 1; z++) {
                         BlockPos testPos = new BlockPos(position.add(x, y, z));
-                        if (worldServer.getBlockState(testPos).isFullBlock()) {
-                            worldServer.setBlockToAir(position.add(x, y, z));
+                        if (this.world.getBlockState(testPos).isFullBlock()) {
+                            this.world.setBlockToAir(position.add(x, y, z));
                         }
                     }
                 }
